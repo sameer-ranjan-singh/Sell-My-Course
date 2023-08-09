bbd03f5ea9aba7aa29b6c6f41fb18b5c7fa67c2b
Sameer Ranjan Singh <sameer.lancer99@gmail.com>
2023-08-09 19:13:59 +0530

    server : File structuring , .env

---
+++ server/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const mongoUrl = process.env.MONGO_URL
+const mongoose = require("mongoose")
+const mongoOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "courses"
+  };
+
+const express = require("express")
+const cors = require("cors")
+
+const adminRouter = require("./routes/admin")
+const userRouter = require("./routes/user")
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.use("/admin",adminRouter)
+// app.use("/user",userRouter)
+
+mongoose.connect(mongoUrl, mongoOptions);
+
+app.listen(3000, () => console.log("Server is running on port 3000"))
---
+++ server/routes/admin.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const { SECRET } = require("../middleware/auth")
+const { authenticateJwt } = require("../middleware/auth")
+const { Admin , User, Course } = require("../db/index.js")
+
+const router = express.Router()
+
+router.get("/me",authenticateJwt , (req , res) => {
+    res.json({
+      username: req.user.username 
+    })
+  })
+  
+  router.post('/signup', (req, res) => {
+    const { username, password } = req.body;
+    function callback(admin) {
+      if (admin) {
+        res.status(403).json({ message: 'Admin already exists' });
+      } else {
+        const obj = { username: username, password: password };
+        const newAdmin = new Admin(obj);
+        newAdmin.save();
+        const token = jwt.sign({ username, role: 'admin' }, SECRET, { expiresIn: '1h' });
+        res.json({ message: 'Admin created successfully', token });
+      }
+  
+    }
+    Admin.findOne({ username }).then(callback);
+  });
+  
+  router.post('/login', async (req, res) => {
+    const { username, password } = req.headers;
+    const admin = await Admin.findOne({ username, password });
+    if (admin) {
+      const token = jwt.sign({ username, role: 'admin' }, SECRET, { expiresIn: '3h' });
+      res.json({ message: 'Logged in successfully', token });
+    } else {
+      res.status(403).json({ message: 'Invalid username or password' });
+    }
+  });
+  
+  router.post('/courses', authenticateJwt, async (req, res) => {
+    const course = new Course(req.body);
+    await course.save();
+    res.json({ message: 'Course created successfully', courseId: course.id });
+  });
+  
+  router.put('/courses/:courseId', authenticateJwt, async (req, res) => {
+    const course = await Course.findByIdAndUpdate(req.params.courseId, req.body, { new: true });
+    if (course) {
+      res.json({ message: 'Course updated successfully' });
+    } else {
+      res.status(404).json({ message: 'Course not found' });
+    }
+  });
+  
+  router.get('/courses', authenticateJwt, async (req, res) => {
+    const courses = await Course.find({});
+    res.json({ courses });
+  });
+  
+  router.get("/course/:courseId",authenticateJwt, async (req,res)=>{
+    const courseId = req.params.courseId ;
+    const course = await Course.findById(courseId)
+    res.json({course})
+  })
+  
+  router.delete("/courses/:courseId", authenticateJwt, async (req,res) => {
+    const courseId = req.params.courseId ;
+    const deletedCourse = await Course.findByIdAndDelete(courseId, req.body, { new: true })
+     try{
+       if(!deletedCourse){
+         res.status(404).json({message:"course not found"})
+        }
+        res.json({message:"Course deleted successfully",deletedCourse})
+     }catch(error){
+      res.status(500).json({ message: 'Error deleting course', error: error.message });
+     }
+  })
+
+  module.exports = router
