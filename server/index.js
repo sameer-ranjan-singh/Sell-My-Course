6f2fd51576809b0cbcf14cbcfea4bc4ff18b7599
Sameer Ranjan Singh <sameer.lancer99@gmail.com>
2023-08-10 12:56:26 +0530

    added : PORT

--- server/index.js
+++ server/index.js
@@ -24,4 +24,4 @@ app.use("/admin",adminRouter)
 
 mongoose.connect(mongoUrl, mongoOptions);
 
-app.listen(3000, () => console.log("Server is running on port 3000"))
+app.listen(process.env.PORT || 3000, () => console.log("Server is running on port 3000"))
